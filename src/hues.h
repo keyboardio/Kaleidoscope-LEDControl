6b6f46d8b537364f9f0bd4f0eee83970cb075bf1
Shriramana Sharma <samjnaa@gmail.com>
2018-06-17 17:51:28 +0530

    add hues.h contain simple hue names for convenience

---
+++ src/hues.h
@@ -0,0 +1,12 @@
+#define HUE_RED            0
+#define HUE_RED_YELLOW    21
+#define HUE_YELLOW        43
+#define HUE_GREEN_YELLOW  64
+#define HUE_GREEN         85
+#define HUE_GREEN_CYAN   107
+#define HUE_CYAN         128
+#define HUE_BLUE_CYAN    149
+#define HUE_BLUE         171
+#define HUE_BLUE_MAGENTA 192
+#define HUE_MAGENTA      213
+#define HUE_RED_MAGENTA  235
